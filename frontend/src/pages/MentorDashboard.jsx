cea849957a2dbe488e974d240790fdd5fcdf4123
JhoelVillca <jhoel.mv.villca@gmail.com>
2026-04-28 21:35:57 -0400

    Reboot

--- frontend/src/pages/MentorDashboard.jsx
+++
@@ -1,8 +0,0 @@
-export default function MentorDashboard() {
-  return (
-    <div className="min-h-screen bg-gray-950 flex flex-col justify-center items-center text-yellow-500">
-      <h1 className="text-5xl font-bold mb-4">Sala de Profesores</h1>
-      <p className="text-xl text-gray-400">Revisa tu agenda de clases y tus pagos pendientes de Stripe.</p>
-    </div>
-  );
-}
